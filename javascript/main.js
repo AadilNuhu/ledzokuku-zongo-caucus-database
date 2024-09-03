38fb1ba513a001bc7a94e1a81cd45a1e368a19e8
Aadil Nuhu <aadilnuhu122@gmail.com>
2024-09-03 10:34:10 -0700

    loader animation has been added

--- javascript/main.js
+++ javascript/main.js
@@ -9,3 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+//Animation
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        // Hide the loader and show the content
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('content').style.display = 'block';
+        // Restore scrolling
+        document.body.style.overflow = 'auto';
+    }, 3000); // 3 seconds
+});
